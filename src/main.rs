072691bbc4d7b326c1d67709225a3a641b9213a0
HaineSensei <81431922+HaineSensei@users.noreply.github.com>
2025-02-18 21:00:08 +0000

    First version complete.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::iter;
+use std::io::{self,Write};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    print!("size: ");
+    io::stdout().flush().unwrap();
+    let mut size = String::new();
+    io::stdin().read_line(&mut size).expect("Expected a string");
+    let size = size[..size.len()-1].parse::<usize>().expect("Expected a non-negative integer.");
+    let mut text : String = iter::repeat(' ').take(size).collect();
+    print!("{text}");
+    io::stdout().flush().unwrap();
+    loop {
+        for c in b"# " {
+            for i in 0..size {
+                unsafe{
+                    text.as_bytes_mut()[i] = *c;
+                    print!("\r{text}");
+                    io::stdout().flush().unwrap();
+                    thread::sleep(Duration::from_millis(5));
+                }
+            }
+        }
+    }   
 }
